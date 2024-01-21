fe36af25cc7efae8f832bacb8cb507c0839b1e99
abdullahosama2311 <156536976+abdullahosama2311@users.noreply.github.com>
2024-01-21 20:15:19 +0400

    Updates

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,11 +1,3 @@
 export default function page () {
     return <p>Articles For Agents</p>
 }
-
-<img
-src="/hero-mobile.png"
-width={1000}
-height={760}
-className="hidden md:block"
-alt="Screenshots of the dashboard project showing desktop version"
-/>
