db93b0fbef2132768d0b6272538140557450b20a
abdullahosama2311 <156536976+abdullahosama2311@users.noreply.github.com>
2024-01-22 19:19:59 +0400

    1

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Children } from 'react';
 
 export default function Page() {
   return (
@@ -38,6 +39,7 @@ export default function Page() {
             <span>Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
+        
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
